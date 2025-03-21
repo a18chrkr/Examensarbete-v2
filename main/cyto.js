dd9d96227edd843bb583270d9d0c506a37d307cf
a18chrkr <102579106+a18chrkr@users.noreply.github.com>
2025-03-21 12:34:08 +0100

    Created Cytoscape.js test render
    Added JavaScript test code provided by Cytoscape.js. Added CSS to support the rendering area. Also had to change the script type to module in the HTML-file in order for the page to recognize the JavaScript as an ES module.

--- main/cyto.js
+++ main/cyto.js
@@ -1,4 +1,4 @@
-import cytoscape from '../node_modules/cytoscape/dist/cytoscape.esm.min.mjs';
+import cytoscape from "../node_modules/cytoscape/dist/cytoscape.esm.min.mjs";
 
 'use strict';
 
@@ -21,3 +21,47 @@ const mergeJSONDatasets = async function(datasets){
     }
     return mergedDatasets;
 }
+
+var cy = cytoscape({
+
+    container: document.getElementById('cy'), // container to render in
+  
+    elements: [ // list of graph elements to start with
+      { // node a
+        data: { id: 'a' }
+      },
+      { // node b
+        data: { id: 'b' }
+      },
+      { // edge ab
+        data: { id: 'ab', source: 'a', target: 'b' }
+      }
+    ],
+  
+    style: [ // the stylesheet for the graph
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#666',
+          'label': 'data(id)'
+        }
+      },
+  
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'line-color': '#ccc',
+          'target-arrow-color': '#ccc',
+          'target-arrow-shape': 'triangle',
+          'curve-style': 'bezier'
+        }
+      }
+    ],
+  
+    layout: {
+      name: 'grid',
+      rows: 1
+    }
+  
+  });
