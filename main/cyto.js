c5abfafef541d80daab6810fa925907bde3959e2
a18chrkr <102579106+a18chrkr@users.noreply.github.com>
2025-03-25 10:53:53 +0100

    Updated renderer function and formatted file
    Change the renderer file to correctly render the datasets and not the test code gathered from cytoscapes website. Also formatted the file

--- main/cyto.js
+++ main/cyto.js
@@ -6,23 +6,23 @@ import cytoscape from "../node_modules/cytoscape/dist/cytoscape.esm.min.mjs";
 const datasets = ['../json_parser/tp73.json', '../json_parser/abl1.json'];
 
 // Use the array of datasets and merge them into one
-const mergeJSONDatasets = async function(datasets){
+const mergeJSONDatasets = async function (datasets) {
     let mergedDatasets = [];
 
-    for(let dataset of datasets){
-        try{
+    for (let dataset of datasets) {
+        try {
             const response = await fetch(dataset);
             const data = await response.json();
             mergedDatasets = mergedDatasets.concat(data)
         }
-        catch(error){
+        catch (error) {
             console.log(`There was an error fetching ${dataset}: `, error);
         }
     }
     return mergedDatasets;
 }
 
-const convertToCytoscape = function(biogridData){
+const convertToCytoscape = function (biogridData) {
     const nodes = new Map(); // En map säkerställer att endast unika noder renderas
     const edges = [];
     const expSys = new Map(); // Alla experimental systems
@@ -37,7 +37,7 @@ const convertToCytoscape = function(biogridData){
         if (!nodes.has(interactorA)) nodes.set(interactorA, { data: { id: interactorA, name: interactorA, throughput, experimentalSystem } });
         if (!nodes.has(interactorB)) nodes.set(interactorB, { data: { id: interactorB, name: interactorB, throughput, experimentalSystem } });
 
-        if(!expSys.has(experimentalSystem)) expSys.set(experimentalSystem, experimentalSystem)
+        if (!expSys.has(experimentalSystem)) expSys.set(experimentalSystem, experimentalSystem)
 
         // Lägger till en edge (relation mellan två noder)
         edges.push({
@@ -51,7 +51,7 @@ const convertToCytoscape = function(biogridData){
         });
     });
 
-    console.log(`Noder ${nodes.size}, Edges ${edges.length}}`)
+    console.log(`Noder ${nodes.size}, Edges ${edges.length}}`) // Loggar totalt antal noder och länkar/edges
     console.log(expSys)
     return [...nodes.values(), ...edges]; // Separerar och kombinerar node map och edge array till en enhetlig array
 }
@@ -60,46 +60,37 @@ mergeJSONDatasets(datasets).then(datasets => {
     const elements = convertToCytoscape(datasets)
 })
 
-var cy = cytoscape({
-
-    container: document.getElementById('cy'), // container to render in
-  
-    elements: [ // list of graph elements to start with
-      { // node a
-        data: { id: 'a' }
-      },
-      { // node b
-        data: { id: 'b' }
-      },
-      { // edge ab
-        data: { id: 'ab', source: 'a', target: 'b' }
-      }
-    ],
-  
-    style: [ // the stylesheet for the graph
-      {
-        selector: 'node',
-        style: {
-          'background-color': '#666',
-          'label': 'data(id)'
+const renderCytoscape = function (elements) {
+    cytoscape({
+        container: document.getElementById("cy"), // Pekar på DOM elementet där renderingen sker
+        elements: elements,
+        style: [
+            {
+                selector: "node", // Standard färg på en nod
+                style: {
+                    "label": "data(name)",
+                    "background-color": "#0074D9", // Blå
+                    "height": "30px",
+                    "width": "30px",
+                }
+            },
+            {
+                selector: "node[throughput = 'High Throughput']", // Ändrar färgen till röd vid hög throughput
+                style: {
+                    "background-color": "#ff6666", // Röd
+                }
+            },
+            {
+                selector: "edge",
+                style: {
+                    "width": 2,
+                    "line-color": "gray",
+                }
+            },
+        ],
+        layout: {
+            name: "concentric",
+            animate: false,
         }
-      },
-  
-      {
-        selector: 'edge',
-        style: {
-          'width': 3,
-          'line-color': '#ccc',
-          'target-arrow-color': '#ccc',
-          'target-arrow-shape': 'triangle',
-          'curve-style': 'bezier'
-        }
-      }
-    ],
-  
-    layout: {
-      name: 'grid',
-      rows: 1
-    }
-  
-  });
+    });
+}
