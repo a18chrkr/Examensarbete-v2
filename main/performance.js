885debb034689512f351bd8850e3777792828e1e
a18chrkr <102579106+a18chrkr@users.noreply.github.com>
2025-04-01 16:40:41 +0200

    Added functionality for performance.js to gather data from CDN resources
    Due to challanges with locally installing Sigma.js I had to deal with CDN's and the resources needed to load their data. Added code functionality should solve this problem and correctly prepare it for CSV download. Might hade too look over potential error occuring with the Cytoscape script going forward.

--- main/performance.js
+++ main/performance.js
@@ -10,15 +10,50 @@ export const startPerformanceObserver = function (reloads = 10, delay = 500) {
 
         setTimeout(() => {
             const perf = performance.getEntriesByType('navigation')[0]?.toJSON();
+            const resources = performance.getEntriesByType("resource");
 
             if (!perf) {
                 console.log('There was an error measuring performance.');
                 return;
             }
 
+            // Get data from navigation api
             measurement['loadTime'] = (perf.loadEventEnd - perf.loadEventStart).toFixed(2);
             measurement['totalLoadTime'] = (perf.loadEventEnd - perf.startTime).toFixed(2);
             measurement['domContentLoadedTime'] = (perf.domContentLoadedEventEnd - perf.domContentLoadedEventStart).toFixed(2);
+
+            // Only saves cdn's'
+            const cdnResources = resources.filter(resource =>
+                resource.name.includes('cdn') && resource.name.includes('cloudflare')
+            );
+
+            // Set each resource as key and load time as value
+            cdnResources.forEach(resource => {
+                let resourceName;
+                if (resource.name === 'https://cdnjs.cloudflare.com/ajax/libs/sigma.js/2.4.0/sigma.min.js')
+                    resourceName = 'sigmaLoadTime'
+                else if (resource.name === 'https://cdnjs.cloudflare.com/ajax/libs/graphology/0.25.4/graphology.umd.min.js') {
+                    resourceName = 'graphologyLoadTime'
+                }
+                measurement[resourceName] = (resource.duration).toFixed(2);
+            })
+            console.log(cdnResources)
+
+            // collect earliest startTime of CDN
+            if (cdnResources[0].startTime < cdnResources[1].startTime) {
+                measurement['earliestResourceStartTime'] = cdnResources[0].startTime;
+            }
+            else {
+                measurement['earliestResourceStartTime'] = cdnResources[1].startTime;
+            }
+
+            // collect latest responseEnd
+            if (cdnResources[0].responseEnd > cdnResources[1].responseEnd) {
+                measurement['latestResourceResponseEnd'] = cdnResources[0].responseEnd;
+            }
+            else {
+                measurement['latestResourceRespondEnd'] = cdnResources[1].responseEnd;
+            }
 
             measurements.push(measurement);
             localStorage.setItem('measurements', JSON.stringify(measurements));
@@ -40,8 +75,9 @@ export const startPerformanceObserver = function (reloads = 10, delay = 500) {
                 measurements = measurements.map(measurements => Object.values(measurements))
 
                 // Adds a row with headers for csv file
-                measurements.unshift(['loadTime', 'totalLoadTime', 'domContentLoadTime'])
+                measurements.unshift(['loadTime', 'totalLoadTime', 'domContentLoadTime', 'cdnResource', 'earliestResourceStartTime', 'latestResourceResponseEnd'])
 
+                // CSV content
                 const csvMeasurements = "data:text/csv;charset=utf-8," + measurements.map(row => row.join(',')).join('\n');
                 const encodedMeasurements = encodeURI(csvMeasurements);
 
@@ -51,6 +87,7 @@ export const startPerformanceObserver = function (reloads = 10, delay = 500) {
                 link.setAttribute("download", "measurements.csv"); // Name of csv file
                 document.body.appendChild(link);
                 link.click(); // Auto clicks the link to download the csv
+                document.body.removeChild(link);
 
                 localStorage.removeItem('reloadCount'); // Reset iteration counter
                 localStorage.removeItem('measurements'); // Remove all measurements 
